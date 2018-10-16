5a2badac8312607c15cf4b0558535858014370db
Segger87 <Segger87@gmail.com>
2018-10-16 22:47:48 +0100

    further styling and animation additions

--- src/app/gaming/gaming.component.ts
+++ src/app/gaming/gaming.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { slideToBottom } from '../router.animations';
 
 @Component({
   selector: 'app-gaming',
   templateUrl: './gaming.component.html',
-  styleUrls: ['./gaming.component.css']
+  styleUrls: ['./gaming.component.css'],
+  animations: [slideToBottom()]
 })
 export class GamingComponent implements OnInit {
 
--- src/app/piano/piano.component.ts
+++ src/app/piano/piano.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { slideToLeft, slideToBottom } from '../router.animations';
+import { slideToBottom } from '../router.animations';
 
 @Component({
   selector: 'app-piano',
   templateUrl: './piano.component.html',
-  styleUrls: ['./piano.component.css'],
+  styleUrls: ['./piano.component.scss'],
   animations: [slideToBottom()]
 })
 export class PianoComponent implements OnInit {
