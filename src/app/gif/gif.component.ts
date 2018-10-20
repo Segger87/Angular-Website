f9c88dd1dd141206902a041ae2b8912709f7ae08
Segger87 <Segger87@gmail.com>
2018-10-20 15:54:33 +0100

    added timestamp for api calls so that the component only requests the service every 60 seconds

--- src/app/gif/gif.component.ts
+++ src/app/gif/gif.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GifService } from '../gif.service';
-import { slideToRight } from '../router.animations';
-import { slideToLeft } from '../router.animations';
+import { slideToRight, slideToLeft } from '../router.animations';
 
 @Component({
  selector: 'app-gif',
@@ -12,34 +11,44 @@ import { slideToLeft } from '../router.animations';
 export class GifComponent implements OnInit {
 
  public urls: string[];
+ public time: Date;
 
  constructor(private _gifService: GifService) { }
 
  ngOnInit() {
-  const setInSessionStorage = this.getStorage();
+  const setInSessionStorage = this.getStorageData();
+  const timeOfLastRetrieval = this.getStorageTime();
 
-  if (setInSessionStorage.length == 0) {
+  if (setInSessionStorage.length == 0 || Date.now() - Date.parse(timeOfLastRetrieval) > 60000) { //hits api every 60seconds 
     this._gifService.retrieveGifData().subscribe(json => {
       this.urls = [];
+      this.time = new Date();
       json.data.forEach(datum => {
         this.urls.push(datum.images.downsized.url);
         setInSessionStorage.push(datum.images.downsized.url);
-        this.setStorage(setInSessionStorage);
+        this.setStorage(setInSessionStorage, this.time);
         console.log('set');
       });
     });
   } else {
-    this.urls = this.getStorage();
+    this.urls = this.getStorageData();
     console.log('get');
+    console.log(Date.now() - Date.parse(timeOfLastRetrieval) + 'seconds elapsed since last API hit');
   }
  }
 
- public getStorage(): string[] {
+ public getStorageData(): string[] {
    const sessionStorageItem = JSON.parse(sessionStorage.getItem('urls'));
    return sessionStorageItem == null ? [] : sessionStorageItem.urls;
  }
 
- private setStorage(urls: string[]): void {
+ public getStorageTime(): string {
+  const sessionStorageTime = JSON.parse(sessionStorage.getItem('timeSet'));
+  return sessionStorageTime == null ? "" : sessionStorageTime.time;
+}
+
+ private setStorage(urls: string[], timeRetrieved: Date): void {
   sessionStorage.setItem('urls', JSON.stringify({urls: urls}));
+  sessionStorage.setItem('timeSet', JSON.stringify({ time: timeRetrieved}));
  }
 }
